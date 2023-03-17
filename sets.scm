a6b37e3edc88f8188f6fc453dd6c59c4a1bd3f86
xlxs4 <orousoultzoglou@gmail.com>
2023-03-17 03:20:15 +0200

    Ordered set operations

--- sets.scm
+++ sets.scm
@@ -15,10 +15,8 @@
          '())
         ((element-of-set? (car set1) set2)
          (cons (car set1)
-               (intersection-set (cdr set1)
-                                 set2)))
-        (else (intersection-set (cdr set1)
-                                set2))))
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
 
 (define (u/union-set set1 set2)
   (fold-right adjoin-set set2 set1))
@@ -34,3 +32,40 @@
 
 (define (um/union-set set1 set2)
   (append set1 set2))
+
+(define (o/element-of-set? x set)
+  (cond ((null? set) #f)
+        ((= x (car set)) #t)
+        ((< x (car set)) #f)
+        (else (element-of-set? x (cdr set)))))
+
+(define (o/adjoin-set x set)
+  (cond ((or? (null? set) (< x (car set)))
+         (cons x set))
+        ((= x (car set)) set)
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (o/intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1 (intersection-set (cdr set1)
+                                          (cdr set2))))
+              ((< x1 x2) (intersection-set (cdr set1)
+                                           set2))
+              ((< x2 x1) (intersection-set set1 (cdr set2)))))))
+
+(define (o/union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+         (let ((x1 (car set1))
+               (x2 (car set2)))
+           (cond ((= x1 x2)
+                  (cons x1 (o/union-set (cdr set1) (cdr set2))))
+                 ((< x1 x2)
+                  (cons x1 (o/union-set (cdr set1) set2)))
+                 (else
+                  (cons x2 (o/union-set set1 (cdr set2)))))))))
