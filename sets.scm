183719b3e8dfd9e27e6fba7b3b65f44cdac877e8
xlxs4 <orousoultzoglou@gmail.com>
2023-03-17 19:16:16 +0200

    Balanced binary tree operations

--- sets.scm
+++ sets.scm
@@ -69,3 +69,85 @@
                   (cons x1 (o/union-set (cdr set1) set2)))
                  (else
                   (cons x2 (o/union-set set1 (cdr set2)))))))))
+
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (tr/element-of-set? x set)
+  (cond ((null? set) #f)
+        ((= x (entry set)) #t)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define (tr/adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree
+          (entry set)
+          (adjoin-set x (left-branch set))
+          (right-branch set)))
+        ((> x (entry set))
+         (make-tree
+          (entry set)
+          (left-branch set)
+          (adjoin-set x (right-branch set))))))
+
+(define (list->tree elements)
+  (car (partial-tree
+        elements (length elements))))
+
+(define (tree->list tree)
+  (if (null? tree)
+      '()
+      (append (tree->list (left-branch tree))
+              (cons (entry tree)
+                    (tree->list (right-branch tree))))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size
+             (quotient (- n 1) 2)))
+        (let ((left-result
+               (partial-tree
+                elts left-size)))
+          (let ((left-tree
+                 (car left-result))
+                (non-left-elts
+                 (cdr left-result))
+                (right-size
+                 (- n (+ left-size 1))))
+            (let ((this-entry
+                   (car non-left-elts))
+                  (right-result
+                   (partial-tree
+                    (cdr non-left-elts)
+                    right-size)))
+              (let ((right-tree
+                     (car right-result))
+                    (remaining-elts
+                     (cdr right-result)))
+                (cons (make-tree this-entry
+                                 left-tree
+                                 right-tree)
+                      remaining-elts))))))))
+
+(define (tr/intersection-set set1 set2)
+  (let ((lset1 (tree->list set1))
+        (lset2 (tree->list set2))
+        (lintersection (o/intersection-set lset1 lset2))
+        (intersection (list->tree lintersection)))
+    intersection))
+
+(define (tr/union-set set1 set2)
+  (let ((lset1 (tree->list set1))
+        (lset2 (tree->list set2))
+        (lunion (o/union-set lset1 lset2))
+        (union (list->tree lunion)))
+    union))
